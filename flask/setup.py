97f4aabbe182cee093ce6d5ebb1b129ab3d27e13
Luan Louzada <luanlouzadadev@gmail.com>
2023-10-25 13:16:31 -0300

    base template

---
+++ flask/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="flask_blog",
+    version='0.1.0',
+    packages=["blog"],
+    author="Luan Louzada",
+    install_requires=["flask", "flask_pymongo", "dynaconf", "unidecode"],
+)
